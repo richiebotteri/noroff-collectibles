fe9cfb51083466f4c80067a55b24dd4d1897fe2f
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-17 12:04:23 +0100

    adds api env constance

---
+++ src/js/api/env.mjs
@@ -0,0 +1,6 @@
+// Setting up API environment variables
+
+export const HOST_URL = "https://nf-api.onrender.com";
+export const BASE_PATH = "/api/v1";
+export const AUCTIONS_ROUTE = "/auction";
+export const AUCTIONS_URL = `${HOST_URL}${BASE_PATH}${AUCTIONS_ROUTE}`;
