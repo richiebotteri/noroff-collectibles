33e6823e08dd0edfa50db244e64db0384293c06b
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-19 19:02:30 +0100

    adds dynamic auction page lisings

---
+++ src/js/components/cards/auction-page-listing.mjs
@@ -0,0 +1,32 @@
+import { createHtmlElement } from "../../helpers/methods/create-element.mjs";
+import { createHtmlMainImage } from "../card-components/card-elements/main-image.mjs";
+import { createTotalListingHtml } from "../card-components/card-elements/total-listings.mjs";
+
+import { createCardBody } from "../card-components/card-body.mjs";
+
+export function createAuctionPageListings(ApiListingData, listingIndex) {
+   // Deconstruct API listing data
+   const { id, title, description, media, tags, created, updated, endsAt, _count, bids, seller } = ApiListingData;
+
+   // Get index page listing container and total listings title
+   const listingAuctionGrid = document.querySelector("#listing-auction-grid");
+   const totalListingsContainer = document.querySelector("#listing-count");
+
+   // Create card elements
+   const totalListingParagraph = createTotalListingHtml(listingIndex);
+
+   const cardLinkContainer = createHtmlElement("a");
+   cardLinkContainer.href = "/listing-details/";
+   cardLinkContainer.classList.add("card-listing-y", "g-col-12", "g-col-md-6", "g-col-lg-4", "d-block");
+
+   const htmlImage = createHtmlMainImage(media, title);
+   htmlImage.classList.add("img-thumbnail");
+
+   const cardBody = createCardBody(title, description, tags, endsAt, bids);
+
+   // Construct Auction Page Card
+   cardLinkContainer.appendChild(htmlImage);
+   cardLinkContainer.appendChild(cardBody);
+   listingAuctionGrid.appendChild(cardLinkContainer);
+   totalListingsContainer.replaceChildren(totalListingParagraph);
+}
