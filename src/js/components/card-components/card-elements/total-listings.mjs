119d206ef66ea292990503dfd9915a88f0aa3aa3
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-20 10:42:18 +0100

    adds correct styling for index, auction, login and register page

--- src/js/components/card-components/card-elements/total-listings.mjs
+++ src/js/components/card-components/card-elements/total-listings.mjs
@@ -1,8 +1,9 @@
 import { createHtmlElement } from "../../../helpers/methods/create-element.mjs";
 
 export function createTotalListingHtml(totalListings) {
+   const correctedNumber = totalListings + 1;
    const paragraph = createHtmlElement("p");
    paragraph.classList.add("text-capitalize", "fs-4");
-   paragraph.innerText = `Total (${totalListings})`;
+   paragraph.innerText = `Total (${correctedNumber})`;
    return paragraph;
 }
