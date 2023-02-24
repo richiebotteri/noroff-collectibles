6ce3aac6e4d07a69db6c94e64c3e19a03d733dae
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-24 18:03:19 +0100

    fixes total listings counter

--- src/js/components/card-components/card-elements/total-listings.mjs
+++ src/js/components/card-components/card-elements/total-listings.mjs
@@ -1,7 +1,7 @@
 import { createHtmlElement } from "../../../helpers/methods/create-element.mjs";
 
 export function createTotalListingHtml(totalListings) {
-   const correctedNumber = totalListings + 1;
+   const correctedNumber = totalListings;
    const paragraph = createHtmlElement("p");
    paragraph.classList.add("text-capitalize", "fs-4");
    paragraph.innerText = `Total (${correctedNumber})`;
