78f969c01b5cc96a7fe3cfa1ea134bfb86c68887
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-24 14:44:31 +0100

    isolating variables only for home page

--- src/js/components/route-listing-data.mjs
+++ src/js/components/route-listing-data.mjs
@@ -6,23 +6,24 @@ import { createSearchResult } from "./search/create-search-result.mjs";
 
 export function routeListingData(listingObjects, searchValue) {
    listingObjects.forEach((apiListingObject, index) => {
-      const { seller } = apiListingObject;
-      const sellerName = seller.name;
-
-      const profile = loadItem("profile");
-      const profileName = profile.name;
-
       // Route data to create card components
 
       const path = document.location.pathname;
       if (path === "/index.html" || path === "/Semester-Project-2/index.html") {
          createCarouselListing(apiListingObject, index);
-      } else if (path === "/home.html" && sellerName === profileName) {
-         createGridListing(apiListingObject, index);
       } else if (path === "/auctions-page.html" || path === "/Semester-Project-2/auctions-page.html") {
          createGridListing(apiListingObject, index);
       } else if (path === "/listing-page.html" || path === "/Semester-Project-2/listing-page.html") {
          createListingPageContent(apiListingObject, index);
+      } else if (path === "/home.html") {
+         const { seller } = apiListingObject;
+         const sellerName = seller.name;
+
+         const profile = loadItem("profile");
+         const profileName = profile.name;
+         if (sellerName === profileName) {
+            createGridListing(apiListingObject, index);
+         }
       }
 
       if (searchValue) {
