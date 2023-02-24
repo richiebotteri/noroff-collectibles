3dd3e93ed850305362d8b8b7aa90dffd8d26b0dc
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-24 14:42:34 +0100

    refactors and adds profile to storage deletion

--- src/js/api/auth/delete-storage-signing-out.mjs
+++ src/js/api/auth/delete-storage-signing-out.mjs
@@ -1,10 +1,11 @@
 import { deleteItem } from "../../storage/localStorage/delete.mjs";
 
-export function deleteTokenSigningOut() {
+export function deleteStorageSigningOut() {
    document.addEventListener("DOMContentLoaded", () => {
       const logoutBtn = document.querySelector("#logout");
       logoutBtn.addEventListener("click", () => {
          deleteItem("token");
+         deleteItem("profile");
       });
    });
 }
--- src/js/api/auth/if-token-do-task.mjs
+++ src/js/api/auth/if-token-do-task.mjs
@@ -1,16 +1,15 @@
 import { loadItem } from "../../storage/localStorage/load.mjs";
 import { changeToSignedInLinks } from "./change-to-signed-in-links.mjs";
-import { deleteTokenSigningOut } from "./delete-token-signing-out.mjs";
+import { deleteStorageSigningOut } from "./delete-storage-signing-out.mjs";
 
 export function ifTokenDoTask() {
    const isToken = loadItem("token");
    const path = location.pathname;
 
    if (isToken) {
-      deleteTokenSigningOut();
+      deleteStorageSigningOut();
       if (path === "/auctions-page.html") {
          changeToSignedInLinks();
       }
-   } else {
    }
 }
