30314dba6445f4c059e0e2541b1cd80bf0c054e4
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-02-26 16:01:37 +0100

    adding new function api data formating

---
+++ src/js/api/format-fetch-data.mjs
@@ -0,0 +1,46 @@
+import { formatStringData } from "../helpers/format-string-data.mjs";
+import { formatBids } from "../helpers/methods/format-bids.mjs";
+import { formatDate } from "../helpers/methods/format-date.mjs";
+import { removeEmptyArray } from "../helpers/methods/remove-empty-array.mjs";
+import { trimArray } from "../helpers/methods/trim-array.mjs";
+import { routeListingData } from "./route-listing-data.mjs";
+
+export function formatFetchData(apiDataArray) {
+   apiDataArray.forEach((apiData, index) => {
+      const { id, title, description, media, tags, created, updated, endsAt, _count, bids, seller } = apiData;
+      // String
+
+      const formattedTitle = formatStringData(title);
+      const formattedDesc = formatStringData(description);
+
+      const removedEmptyTags = removeEmptyArray(tags);
+      const trimmedTagsArray = trimArray(removedEmptyTags);
+
+      const mediaArray = removeEmptyArray(media);
+
+      const createDate = formatDate(created);
+      const endsAtDate = formatDate(endsAt);
+      const updatedDate = formatDate(updated);
+
+      const bidsCount = _count.bids;
+
+      const formattedBids = formatBids(bids);
+
+      const formattedApiData = {
+         index: index,
+         id: id,
+         title: formattedTitle,
+         description: formattedDesc,
+         tagsArray: trimmedTagsArray,
+         mediaArray: mediaArray,
+         createdDate: createDate,
+         endsAtDate: endsAtDate,
+         updatedDate: updatedDate,
+         bidsCount: bidsCount,
+         bids: formattedBids,
+         seller: seller,
+      };
+
+      routeListingData(formattedApiData);
+   });
+}
