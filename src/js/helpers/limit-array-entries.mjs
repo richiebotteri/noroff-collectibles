7647b456a72a0109cc3220382ce15f892f9b38bb
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-03-01 16:33:31 +0100

    isolates button to call on auctions-page

--- src/js/helpers/limit-array-entries.mjs
+++ src/js/helpers/limit-array-entries.mjs
@@ -6,8 +6,11 @@ export function limitArrayEntries(array, maxListing = 10) {
    const minListing = maxListing - numberOfListings;
 
    if (numberOfListings === maxListing) {
-      const loadMoreBtn = document.querySelector("#load-more");
-      loadMoreBtn.classList.replace("d-none", "d-block");
+      const path = location.pathname;
+      if (path === "/auctions-page") {
+         const loadMoreBtn = document.querySelector("#load-more");
+         loadMoreBtn.classList.replace("d-none", "d-block");
+      }
 
       const first10Listings = array.slice(0, numberOfListings);
       return first10Listings;
