6b963d3c234e1856008d1cda686112d2ec938218
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-03-01 16:27:30 +0100

    updates load more feature

--- src/js/helpers/limit-array-entries.mjs
+++ src/js/helpers/limit-array-entries.mjs
@@ -2,11 +2,13 @@ export function limitArrayEntries(array, maxListing = 10) {
    // First 10 posts
    const numberOfListings = 10;
 
-   console.log(maxListing);
    //  Sending out 10 new posts
    const minListing = maxListing - numberOfListings;
 
    if (numberOfListings === maxListing) {
+      const loadMoreBtn = document.querySelector("#load-more");
+      loadMoreBtn.classList.replace("d-none", "d-block");
+
       const first10Listings = array.slice(0, numberOfListings);
       return first10Listings;
    } else {
