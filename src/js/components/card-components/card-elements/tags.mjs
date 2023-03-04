a274d34bd1fd1921cb25f1714f688c5d47807cb4
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-03-04 21:59:09 +0100

    adds  bigger tag font

--- src/js/components/card-components/card-elements/tags.mjs
+++ src/js/components/card-components/card-elements/tags.mjs
@@ -4,7 +4,7 @@ export function createHtmlTags(apiTags) {
    if (Array.isArray(apiTags)) {
       htmlTags = apiTags.map((item) => {
          const htmlSpan = document.createElement("div");
-         htmlSpan.classList.add("data-field--tag");
+         htmlSpan.classList.add("data-field--tag", "fs-5", "text-capitalize");
          htmlSpan.innerText = item;
          return htmlSpan;
       });
