19925a25b251a8619e9f54590310cc274a9c0a21
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-03-05 15:36:48 +0100

    adds create listing validation msg

--- src/js/api/requests/create-listing.mjs
+++ src/js/api/requests/create-listing.mjs
@@ -6,13 +6,27 @@ export async function createListing(submitFormData) {
       const { method, action, ...formData } = submitFormData;
       const response = await fetch(`${AUCTIONS_URL}${action}`, tokenOption(method, formData));
       const result = await response.json();
+
+      const failMsgContainer = document.querySelector("#api-fail-msg");
+      const failMsgText = document.querySelector("#api-fail-msg-text");
+
       if (response.status === 201) {
-         const successMsg = document.querySelector("#api-success-msg");
+         const createSuccessMsg = document.querySelector("#create-success-msg");
          successMsg.classList.replace("d-none", "d-block");
 
          setTimeout(() => {
             location.pathname = "/Semester-Project-2/profile.html";
          }, 1500);
+      }
+
+      // Profile already exist
+      if (response.status === 400) {
+         failMsgContainer.classList.replace("d-none", "d-block");
+         const responseErrorText = result.errors[0].message;
+         failMsgText.innerText = responseErrorText;
+         setTimeout(() => {
+            failMsgContainer.classList.replace("d-block", "d-none");
+         }, 3500);
       }
    } catch (error) {
       console.log(error);
