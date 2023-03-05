86f54d9283589aa07bb26588e9c79b2d7be0f3cf
Richie Botteri <95284314+richiebotteri@users.noreply.github.com>
2023-03-05 11:54:27 +0100

    adds more functions to index js for cleaner look

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -1,39 +1,45 @@
 import { loginForm } from "./listeners/forms/login-form.mjs";
 import { registerForm } from "./listeners/forms/register-form.mjs";
 import { createListingForm } from "./listeners/forms/create-listing-form.mjs";
-import { getListings } from "./api/requests/get-listings.mjs";
 import { searchForm } from "./listeners/forms/search-form.mjs";
-
-import { hasToken } from "./api/auth/has-token.mjs";
 import { createListingPageContent } from "./components/listing-page.mjs";
 import { avatarForm } from "./listeners/forms/update-avatar.mjs";
 import { bidForm } from "./listeners/forms/bid-form.mjs";
 import { changeInputDate } from "./helpers/change-input-date.mjs";
 import { addMoreListings } from "./helpers/add-more-listings.mjs";
 import { toggleSearchDisplay } from "./components/search/toggle-display.mjs";
+import { getProfileAndListings } from "./api/requests/get-profile-and-listings.mjs";
+import { changeElements } from "./api/auth/change-elements.mjs";
+import { deleteLoggingOut } from "./api/auth/delete-logging-out.mjs";
+import hasToken from "./helpers/has-token.mjs";
+import path from "./helpers/path.mjs";
+import { showOfflineMsg } from "./api/auth/show-offline-msg.mjs";
 
-const path = location.pathname;
-
-if (path === "/Semester-Project-2/login-page.html" || path === "/Semester-Project-2/forms.html") {
+// Only evoked on specific pages
+if (path() === "/login-page.html" || path() === "/forms.html") {
    loginForm();
-} else if (path === "/Semester-Project-2/register-page.html") {
+} else if (path() === "/register-page.html") {
    registerForm();
-} else if (path === "/Semester-Project-2/auctions-page.html") {
+} else if (path() === "/auctions-page.html") {
    addMoreListings();
-} else if (path === "/Semester-Project-2/create-listing-page.html") {
+} else if (path() === "/create-listing-page.html") {
    createListingForm();
    changeInputDate();
-} else if (path === "/Semester-Project-2/profile.html") {
+} else if (path() === "/profile.html") {
    avatarForm();
-} else if (path === "/Semester-Project-2/listing-page.html") {
+} else if (path() === "/listing-page.html") {
    bidForm();
    createListingPageContent();
 }
 
-if (path !== "/Semester-Project-2/profile.html") {
-   getListings();
+// Only evoked if token
+if (hasToken()) {
+   changeElements();
+   deleteLoggingOut();
 }
 
+// Evoked on all pages
 searchForm();
 toggleSearchDisplay();
-hasToken();
+getProfileAndListings();
+showOfflineMsg();
